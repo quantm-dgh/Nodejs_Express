7ae3fa3417d6ce294a3876e97927037a975ec92e
thaiquan201198 <thaiquan201198@gmail.com>
2019-12-24 18:14:17 +0700

    initial

---
+++ Lession01_example/db.js
@@ -0,0 +1,8 @@
+var low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync('db.json')
+db = low(adapter)
+db.defaults({ users: [] })
+  .write();
+
+  module.exports = db; 
---
+++ Lession01_example/index.js
@@ -0,0 +1,18 @@
+const express = require ('express')
+const app = express()
+const port = 3000 
+var userRoute = require('./routes/user.route');
+ 
+app.set('view engine', 'pug');
+app.set('views', './views');
+app.use(express.json());// for parsing application/json
+app.use(express.urlencoded({ extended: true })) ;// for parsing application/x-www-form-urlencoded
+
+app.get('/', (req, res) => 
+    res.render('index',{
+        name: 'Minh'
+    }
+));
+app.use('/users', userRoute);
+
+app.listen(port, ()=>console.log(`Example app listening on port ${port}!`)) 
